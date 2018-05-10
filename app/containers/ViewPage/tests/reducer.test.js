38296c97d6d4850a36e09191aeebd4101e1a9983
David Seidenberg <davidseidenberg@gmail.com>
2018-05-10 09:06:10 -0700

    Removed unnecessary tests

--- app/containers/ViewPage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import viewPageReducer from '../reducer';
-
-describe('viewPageReducer', () => {
-  it('returns the initial state', () => {
-    expect(viewPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/ViewPage/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectViewPageDomain } from '../selectors';
-
-describe('selectViewPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
