9766619efc5c5f2de37985cecb40158c2ef114a7
David Seidenberg <davidseidenberg@gmail.com>
2018-05-10 10:42:40 -0700

    Test onChangeEntry

--- app/containers/EntryPage/tests/index.test.js
+++ app/containers/EntryPage/tests/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FormattedMessage, IntlProvider } from 'react-intl';
 import { mount } from 'enzyme';
 
-
+import { changeEntry } from '../actions';
 import { EntryPage, mapDispatchToProps } from '../index';
 import messages from '../messages';
 
@@ -40,6 +40,14 @@ describe('mapDispatchToProps', () => {
       const dispatch = jest.fn();
       const result = mapDispatchToProps(dispatch);
       expect(result.onChangeEntry).toBeDefined();
+    });
+
+    it('should dispatch changeEntry when called', () => {
+      const dispatch = jest.fn();
+      const result = mapDispatchToProps(dispatch);
+      const entry = 'Test Entry';
+      result.onChangeEntry({ target: { value: entry } });
+      expect(dispatch).toHaveBeenCalledWith(changeEntry(entry));
     });
   });
 });
