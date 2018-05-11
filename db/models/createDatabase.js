cf02024c946a9bc3d4ec1f0aaece292f9fe1a409
David Seidenberg <davidseidenberg@gmail.com>
2018-05-10 18:11:46 -0700

    Revert "Add eslint disable for createDatabase error handler"
    This reverts commit 9dac7f43df1e4b258e9d26007e642d16a196d721.

--- db/models/createDatabase.js
+++ db/models/createDatabase.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/journal');
 
-/* eslint-disable no-console */
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
